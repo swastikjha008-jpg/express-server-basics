6185239f8fb535e16e908a1d5c815fe56948ff95
Swastik <swastikjha008@gmail.com>
2026-04-24 23:36:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+console.log("SERVER STARTED");
+app.use(express.json());
+
+// logger middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+let users = [];
+
+// home
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+// get users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// add user
+app.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: "Name is required" });
+  }
+
+  const user = {
+    id: Date.now(),
+    name
+  };
+
+  users.push(user);
+
+  res.status(201).json({
+    message: "User added",
+    user,
+    users
+  });
+});
+
+// delete user
+app.delete("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const initialLength = users.length;
+
+  users = users.filter(user => user.id !== id);
+
+  if (users.length === initialLength) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.json({
+    message: "User deleted",
+    users
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
